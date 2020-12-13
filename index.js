4b584a5418501af167b0cd93b331b79a09c1e405
Kazuki Onishi <princeontrojanhorse@gmail.com>
2020-12-13 10:41:26 +0900

    :tada: init

---
+++ index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+useLocalStorageState = (defaultValue, key) => {
+    const [value, setValue] = React.useState(() => {
+        const stickyValue = window.localStorage.getItem(key);
+        return stickyValue !== null
+            ? JSON.parse(stickyValue)
+            : defaultValue;
+    });
+    React.useEffect(() => {
+        window.localStorage.setItem(key, JSON.stringify(value));
+    }, [key, value]);
+    return [value, setValue];
+}
+
+module.exports = {
+    useLocalStorageState: useLocalStorageState
+};
